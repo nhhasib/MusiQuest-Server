80455a0f80526ac764cb246fdbef4a12146b6d17
nhhasib <nafiul.hasan1997@gmail.com>
2023-06-13 04:11:57 +0600

    dashboard/classes created

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 5000||process.env.PORT
+const express = require("express");
+var cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = 5000 || process.env.PORT;
 
+app.use(express.json());
+app.use(cors());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://MusiQuest:71zWe9DUSL554MyR@cluster0.uld9vql.mongodb.net/?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://MusiQuest:71zWe9DUSL554MyR@cluster0.uld9vql.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -14,28 +16,52 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const usersCollection = client.db("MusiQuest").collection("users");
+    const classesCollection = client.db("MusiQuest").collection("Classes");
+    const instructorsCollection = client
+      .db("MusiQuest")
+      .collection("Instructors");
+
+    app.post("/users", async (req, res) => {
+      const data = req.body;
+      const query = { email: data.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+      const result = await usersCollection.insertOne(data);
+      res.send(result);
+    });
+
+    app.get("/classes", async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('MusiQuest Running')
-})
+app.get("/", (req, res) => {
+  res.send("MusiQuest Running");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
