aaeb2a9292d2e2ac0655eafb21d9b66a319a0b56
nhhasib <nafiul.hasan1997@gmail.com>
2023-06-16 16:47:28 +0600

    final edit

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ async function run() {
             const email = req.decoded.email;
             const query = { email: email };
             const user = await usersCollection.findOne(query);
-            if (user?.role !== 'admin') {
+            if (user?.role !=='admin') {
               return res.status(403).send({ error: true, message: 'forbidden message' });
             }
             next();
